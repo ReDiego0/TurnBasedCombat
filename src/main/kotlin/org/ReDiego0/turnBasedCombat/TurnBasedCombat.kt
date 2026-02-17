ef4170a6940bf2928f9ed856139267a388a42d1e
ReDiego0 <your.email@example.com>
2026-02-16 21:50:14 -0300

    chore: Initial commit

---
+++ src/main/kotlin/org/ReDiego0/turnBasedCombat/TurnBasedCombat.kt
@@ -0,0 +1,12 @@
+package org.ReDiego0.turnBasedCombat
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class TurnBasedCombat : JavaPlugin() {
+
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
